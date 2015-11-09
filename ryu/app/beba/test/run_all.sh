82bac0bb4d8ea6e92b08d964b9602f096f43ab19
lupo89 <luca.pollini@mail.polimi.it>
2015-11-09 06:49:18 -0800

    Automatized tests minor fix

--- ryu/app/beba/test/run_all.sh
+++ ryu/app/beba/test/run_all.sh
@@ -11,13 +11,6 @@ do
 	ret_values[$f]=$?;
 done
 
-for f in *__ryu.py;
-do
-	echo -e "\n\x1b[33m[Testing $f]\x1b[0m";
-	sudo ryu-manager $f;
-	ret_values[$f]=$?;
-done 
-
 echo -e "\n*******************************************************************\n\nSUMMARY:\n"
 
 for f in *.py;
@@ -27,15 +20,6 @@ do
 	  continue
 	fi
 	if [[ ${ret_values[$f]} -eq 1 ]]; then
-		echo -e "$f: \x1b[31mFAILED\x1b[0m";
-	else
-		echo -e "$f: \x1b[32mSUCCEEDED\x1b[0m";
-	fi
-done
-
-for f in *__ryu.py;
-do
-	if [[ ${ret_values[$f]} -eq 137 ]]; then
 		echo -e "$f: \x1b[31mFAILED\x1b[0m";
 	else
 		echo -e "$f: \x1b[32mSUCCEEDED\x1b[0m";
