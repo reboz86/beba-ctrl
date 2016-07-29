23226aab59f76e6ac8b839fc07a653fd28d8ea50
PaNardin <zemelusa@gmail.com>
2016-07-29 17:15:34 +0200

    new indentation of the code

--- ryu/app/beba/README.txt
+++ ryu/app/beba/README.txt
@@ -1,25 +1,25 @@
 BEBA APP Descriptions:
 ----------------------
 
--    nativeMonitoring.py implements:
+-    native_monitoring.py implements:
         
 	MAC learning and ARP forwarding
         IP forwarding: rules inserted for each couple IP src/dst
         monitoring: every <timeInterval>, a FlowStatsReq is sent to the switch for collecting flows' counters
 
--    selectivemonitoring_1.py implements:
+-    selective_monitoring_1.py implements:
     
         Switch flow tables configuration for MAC learning/ARP forwarding
         Counters for four features: -IP Src/Dst and Port Src/Dst
 
--    simplemonitoring_1.py implements:
+-    simple_monitoring_1.py implements:
         
 	Inherit from selectivemonitoring
         Sends requests and receives replies from the switch to get counter values of the features monitored
         Every N seconds (Timewindow) process these counter values into an DDoS entropy-based algorithm in order to detect specific attacks
         If an attack is detected sends flow rules to the switch for the attack mitigation
 
--   simplemonitor.py implements:
+-   simple_monitor.py implements:
 
         Switch flow tables configuration for MAC learning/ARP forwarding
         Combined with an sFlow Agent and Collector
@@ -27,6 +27,6 @@ BEBA APP Descriptions:
         Every N seconds (Timewindow) process these counter values into an DDoS entropy-based algorithm in order to detect specific attacks
         If an attack is detected sends flow rules to the switch for the attack mitigation
 
--   selectivemonitoring.py (same as selectivemonitoring_1.py but for only one feature)
+-   selective_monitoring.py (same as selectivemonitoring_1.py but for only one feature)
 
--   simplemonitoring.py (same as seimplemonitoring_1.py but for only one feature)
+-   simple_monitoring.py (same as seimplemonitoring_1.py but for only one feature)
--- ryu/app/beba/simple_monitoring.py
+++ ryu/app/beba/simple_monitoring.py
@@ -1,6 +1,6 @@
 import logging
 import math
-import selectivemonitoring
+import selective_monitoring
 from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
@@ -10,16 +10,16 @@ import ryu.ofproto.ofproto_v1_3_parser as ofparser
 import ryu.ofproto.beba_v1_0 as bebaproto
 import ryu.ofproto.beba_v1_0_parser as bebaparser
 
-LOG = logging.getLogger('app.openstate.simplemonitoring')
+LOG = logging.getLogger('app.openstate.simple_monitoring')
 
-class SimpleMonitoring(selectivemonitoring.OpenStateSelectiveMonitoring):
 
+class SimpleMonitoring(selective_monitoring.OpenStateSelectiveMonitoring):
     def __init__(self, *args, **kwargs):
         super(SimpleMonitoring, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.monitor_thread = hub.spawn(self._monitor)
-        self.IPsrc = {} # Dictionary: IP src <->  #states
-        self.Entropy_IPsrc = [] # Entropy IP src List 
+        self.ipsrc = {}  # Dictionary: IP src <->  #states
+        self.entropy_ipsrc = []  # Entropy IP src List
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
                 [MAIN_DISPATCHER, DEAD_DISPATCHER])
@@ -39,44 +39,44 @@ class SimpleMonitoring(selectivemonitoring.OpenStateSelectiveMonitoring):
             for dp in self.datapaths.values():
                 self._request_stats(dp)
             hub.sleep(5)
-            entropy = self.entropy(self.IPsrc)
+            entropy = self.entropy(self.ipsrc)
             if (entropy != 0):
-                self.Entropy_IPsrc.append(entropy)
-                LOG.info(self.Entropy_IPsrc)
-            if (len(self.Entropy_IPsrc) >= 6): # Wait 30s before starting  the detection
-                self.detection(self.Entropy_IPsrc,self.IPsrc)
-            self.IPsrc.clear() # Remove all entries in the dictionary
+                self.entropy_ipsrc.append(entropy)
+                LOG.info(self.entropy_ipsrc)
+            if (len(self.entropy_ipsrc) >= 6):  # Wait 30s before starting  the detection
+                self.detection(self.entropy_ipsrc, self.ipsrc)
+            self.ipsrc.clear()  # Remove all entries in the dictionary
 
     def entropy(self, dictionary):
         total_states = 0
         entropy = 0
         p = 0
 
-        for index in dictionary:        
+        for index in dictionary:
             total_states += dictionary[index]
         for index in dictionary:
-            p = float(dictionary[index])/total_states
-            entropy += -p * math.log(p,2)
-        return round(entropy,5)
+            p = float(dictionary[index]) / total_states
+            entropy += -p * math.log(p, 2)
+        return round(entropy, 5)
 
     def detection(self, entropylist, dictionary):
         global threshold_min
-        if (len(entropylist) ==6 ): # Get the entropy of the network under normal conditions during a 30sec window
+        if (len(entropylist) == 6):  # Get the entropy of the network under normal conditions during a 30sec window
             threshold_average = sum(entropylist) / len(entropylist)
             threshold_min = min(entropylist)
         else:
             if (entropylist[-1] < threshold_min):
-                attackerIp = ((max(dictionary, key=dictionary.get))[1:-1]).replace(", ",".")
+                attackerIp = ((max(dictionary, key=dictionary.get))[1:-1]).replace(", ", ".")
                 LOG.info('******* DDoS Flooding  DETECTED *******')
                 LOG.info('Infected Host: %s', attackerIp)
                 for dp in self.datapaths.values():
-                    self.mitigation(dp,attackerIp,0)
+                    self.mitigation(dp, attackerIp, 0)
 
     def mitigation(self, datapath, ipadress, tableid):
-        match = ofparser.OFPMatch(eth_type=0x0800,ipv4_src=ipadress)
+        match = ofparser.OFPMatch(eth_type=0x0800, ipv4_src=ipadress)
         actions = []
         self.add_flow(datapath=datapath, table_id=tableid, priority=100,
-                        match=match, actions=actions)
+                      match=match, actions=actions)
 
     def _request_stats(self, datapath):
         req = bebaparser.OFPExpStateStatsMultipartRequestAndDelete(datapath, table_id=0)
@@ -88,17 +88,17 @@ class SimpleMonitoring(selectivemonitoring.OpenStateSelectiveMonitoring):
         datapath = msg.datapath
 
         if (msg.body.experimenter == 0XBEBABEBA):
-          if(msg.body.exp_type == bebaproto.OFPMP_EXP_STATE_STATS_AND_DELETE):
-            data = msg.body.data
-            state_stats_list = bebaparser.OFPStateStats.parser(data,0)
-            if (state_stats_list!=0):
-                for index in range(len(state_stats_list)):
-                    if (state_stats_list[index].entry.state != 0):
-                        self.IPsrc[str(state_stats_list[index].entry.key)] = state_stats_list[index].entry.state
-            else:
-              LOG.info("No data")
+            if (msg.body.exp_type == bebaproto.OFPMP_EXP_STATE_STATS_AND_DELETE):
+                data = msg.body.data
+                state_stats_list = bebaparser.OFPStateStats.parser(data, 0)
+                if (state_stats_list != 0):
+                    for index in range(len(state_stats_list)):
+                        if (state_stats_list[index].entry.state != 0):
+                            self.ipsrc[str(state_stats_list[index].entry.key)] = state_stats_list[index].entry.state
+                else:
+                    LOG.info("No data")
         # Print the state stats
-        if (len(self.IPsrc)!= 0):
+        if (len(self.ipsrc) != 0):
             LOG.info('****************************')
-        for index in self.IPsrc:
-            LOG.info('IP_SRC=%s State=%s', index, self.IPsrc[index])
+        for index in self.ipsrc:
+            LOG.info('IP_SRC=%s State=%s', index, self.ipsrc[index])
